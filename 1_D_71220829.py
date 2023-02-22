646f54ffe58d2c0fb6692b9d8b8ee288f7008b0f
TimotiusEkanaT <117902158+TimotiusEkanaT@users.noreply.github.com>
2023-02-22 17:45:38 +0700

    Add files via upload

---
+++ 1_D_71220829.py
@@ -0,0 +1,2 @@
+oaushfo = float(input("Masukkan nilai curah hujan : "))
+psodjg = print ("Cuaca terang berawan") if oaushfo == 0 else (print ("Curah hujan ringan") if oaushfo <= 20 else (print("Cuaca hujan sedang") if oaushfo <=50 else (print("Curah hujan lebat") if oaushfo <= 100 else (print("Curah hujan ekstrem") if oaushfo > 100 else () ))))
